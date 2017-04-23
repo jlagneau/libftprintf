7ca85d35997b684e756a3e6bd752839bc574d4f9
jlagneau <jlagneau@student.42.fr>
2017-04-23 15:55:59 +0200

    Add test to compare the custom printf to original

--- tests/src/main.c
+++ tests/src/main.c
@@ -10,34 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <libft.h>
 #include <ft_printf.h>
 #include <locale.h>
 #include "test.h"
 
-int		main(void)
+static void	print_usage(void)
+{
+	ft_dprintf(STDERR, "Usage: ./test (origin|custom)\n");
+}
+
+int		main(int argc, char *argv[])
 {
 	setlocale(LC_CTYPE, "");
-	ft_printf("Hello World !\n");
-	ft_printf("Project done at 42%% !\n");
-	ft_printf("Print double percent %%%%\n");
-	ft_printf("Hello %s !\n", "World");
-	ft_printf("Hello %.6s !\n", NULL);
-	ft_printf("Hello %i World !\n", 42);
-	ft_printf("%d%d\n", 1, 23);
-	ft_printf("42b = %#b\n", 42);
-	ft_printf("42o = %#o\n", 42);
-	ft_printf("42h = %#x\n", 42);
-	ft_printf("42H = %#X\n", 42);
-	ft_printf("ASCII char 42 = %c\n", 42);
-	ft_printf("%c%c%c%c%c\n", 'H', 'e', 'l', 'l', 'o');
-	ft_printf("%p - %p\n", "Hello World", (void*)0);
-	ft_printf("%u %u\n", 2147483648, -1);
-	ft_printf("Hello %s %s%s !\n", "World", "foo", "bar");
-	ft_printf("Hello %10.5d%s%b\n", 42, "foobar", 5);
-	ft_printf("[%*.*s]\n", 10, 2, "Yolo");
-	ft_printf("Print 0 with 0 precision: (%.0d)\n", 0);
-	ft_printf("[%10.4S]\n", L"éàêëâäôîï");
-	ft_printf("[%S] - [%S] - [%S]\n", L"中文", L"العَرَبِيَّة‎‎", L"ру́сский язы́к");
-	ft_printf("[%C][%C]\n", L'中', L'文');
+	if (argc != 2)
+	{
+		print_usage();
+		return (1);
+	}
+	if (ft_strcmp(argv[1], "origin") == 0)
+		origin();
+	else if (ft_strcmp(argv[1], "custom") == 0)
+		custom();
+	else
+	{
+		print_usage();
+		return (1);
+	}	
 	return (0);
 }
---
+++ tests/src/origin.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   origin.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/17 20:30:24 by jlagneau          #+#    #+#             */
+/*   Updated: 2017/04/23 04:12:54 by jlagneau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_printf.h>
+#include <stdio.h>
+#include "test.h"
+
+void	origin(void)
+{
+	printf("Hello World !\n");
+	printf("Project done at 42%% !\n");
+	printf("Print double percent %%%%\n");
+	printf("Hello %s !\n", "World");
+	printf("Hello %.6s !\n", (char*)NULL);
+	printf("Hello %i World !\n", 42);
+	printf("%d%d\n", 1, 23);
+	printf("42o = %#o\n", 42);
+	printf("42h = %#x\n", 42);
+	printf("42H = %#X\n", 42);
+	printf("ASCII char 42 = %c\n", 42);
+	printf("%c%c%c%c%c\n", 'H', 'e', 'l', 'l', 'o');
+	printf("%p - %p\n", "Hello World", (void*)0);
+	printf("%u %u\n", (unsigned int)2147483648, (unsigned int)-1);
+	printf("Hello %s %s%s !\n", "World", "foo", "bar");
+	printf("Hello %10.5d%s\n", 42, "foobar");
+	printf("[%*.*s]\n", 10, 2, "Yolo");
+	printf("Print 0 with 0 precision: (%.0d)\n", 0);
+	printf("[%10.4ls]\n", L"éàêëâäôîï");
+	printf("[%ls] - [%ls] - [%ls]\n", L"中文", L"العَرَبِيَّة", L"ру́сский язы́к");
+	printf("[%lc][%lc]\n", L'中', L'文');
+}
