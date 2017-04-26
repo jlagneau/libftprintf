75acb18eb03d31c1ff79b0317081c8f51190a11d
jlagneau <jlagneau@student.42.fr>
2017-04-26 15:12:00 +0200

    Change structure of the project

--- include/internal/data_struct.h
+++ include/internal/data_struct.h
@@ -1,56 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_defines_internal.h                       :+:      :+:    :+:   */
+/*   data_struct.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/18 09:41:58 by jlagneau          #+#    #+#             */
-/*   Updated: 2017/04/23 15:54:21 by jlagneau         ###   ########.fr       */
+/*   Created: 2017/04/26 15:06:01 by jlagneau          #+#    #+#             */
+/*   Updated: 2017/04/26 15:10:32 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_DEFINES_INTERNAL_H
-# define FT_PRINTF_DEFINES_INTERNAL_H
-# ifdef FT_PRINTF_INTERNAL
-#  include <stddef.h>
-#  include <unistd.h>
-#  include <stdarg.h>
-#  define FT_PRINTF_MAX_LEN	8192
-
-/*
-** Character flags
-**
-**  16 |  8 |  4 |  2 |  1
-** spc |  + |  - |  0 |  #
-*/
-#  define FC_SHARP			0x01
-#  define FC_ZERO			0x02
-#  define FC_MINUS			0x04
-#  define FC_PLUS			0x08
-#  define FC_SPACE			0x10
-
-/*
-** Length modifier flags
-**
-**  32 | 16 |  8 |  4 |  2 |  1
-**   z |  j | ll |  l | hh |  h
-*/
-#  define LM_H				0x01
-#  define LM_HH				0x02
-#  define LM_L				0x04
-#  define LM_LL				0x08
-#  define LM_J				0x10
-#  define LM_Z				0x20
-
-/*
-** Converter specifier flags
-*/
-#  define CS_NUMERIC		0x01
-#  define CS_UNUMERIC		0x02
-#  define CS_SPECIAL		0x04
-#  define CS_CHAR			0x08
-#  define CS_WCHAR			0x10
+#ifndef DATA_STRUCT_H
+# define DATA_STRUCT_H
+# include <stdarg.h>
+# include <stddef.h>
+# include <unistd.h>
 
 typedef struct				s_fc
 {
@@ -78,5 +42,4 @@ typedef struct				s_cs
 	unsigned int			conv_flag;
 }							t_cs;
 
-# endif
 #endif
